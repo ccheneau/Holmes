42b777cef838122fde15ba0cd5521baae2305ca3
Cedric Cheneau <cedric.cheneau@free.fr>
2012-02-06 19:30:01 +0100

    Refactoring

--- holmes-package/src/main/resources/bin/startup.bat
+++ holmes-package/src/main/resources/bin/startup.bat
@@ -7,16 +7,17 @@ set lib_path=%home_path%\lib
 
 rem Set java args
 set java_args=-Dnet.holmes.home="%home_path%" -Dfile.encoding=UTF-8
-rem echo JAVA_ARGS: %java_args%
 
 rem Set classpath
 set class_path=
 for /F %%a in ('dir /b %lib_path%\*.jar') do call :set_class_path %%a
 
 set class_path="%class_path:~1%"
-rem echo CLASS_PATH: %class_path%
 
-"%JAVA_HOME%\bin\java.exe" -classpath %class_path% %java_args% net.holmes.core.MediaServerImpl
+set java=java
+if not "%JAVA_HOME%" == "" set java="%JAVA_HOME%\bin\java.exe"
+
+%java% -classpath %class_path% %java_args% net.holmes.core.HolmesServer
 
 goto :EOF
 
