e6754951d7b1efe84f57bb4c56e105cb9196394b
Cedric Cheneau <cedric.cheneau@free.fr>
2012-07-12 21:35:33 +0200

    Enhence windows startup script

--- holmes-package/src/main/resources/bin/startup.bat
+++ holmes-package/src/main/resources/bin/startup.bat
@@ -1,23 +1,18 @@
 @echo off
 setlocal
 
+rem Set library path
 set bin_path=%~p0
 call :resolve_path "%bin_path%.." home_path
 set lib_path=%home_path%\lib
 
 rem Set java args
-set java_args=-Dnet.holmes.home="%home_path%" -Dfile.encoding=UTF-8 -Xmx30m
-
-rem Set classpath
-set class_path=
-for /F %%a in ('dir /b %lib_path%\*.jar') do call :set_class_path %%a
-
-set class_path="%class_path:~1%"
+set java_args=-Dnet.holmes.home="%home_path%" -Dfile.encoding=UTF-8
 
 set java=java
 if not "%JAVA_HOME%" == "" set java="%JAVA_HOME%\bin\java.exe"
 
-%java% -classpath %class_path% %java_args% net.holmes.core.HolmesServer
+start "Holmes" %java% -Xmx30m %java_args% -jar %lib_path%\holmes-core-${project.version}.jar 
 
 goto :EOF
 
@@ -25,6 +20,3 @@ goto :EOF
 set %2=%~f1
 goto :EOF
 
-:set_class_path
-set class_path=%class_path%;%lib_path%\%1
-goto :EOF
