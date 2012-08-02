5f87c116417b2dce8ffd20b3cd727070a46c9b7f
Cedric Cheneau <cedric.cheneau@free.fr>
2012-08-02 15:13:06 +0200

    Add license header

--- holmes-core/src/test/java/net/holmes/core/configuration/TestConfiguration.java
+++ holmes-core/src/test/java/net/holmes/core/configuration/TestConfiguration.java
@@ -1,3 +1,24 @@
+/**
+* Copyright (c) 2012 Cedric Cheneau
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
 package net.holmes.core.configuration;
 
 import java.io.File;
@@ -23,10 +44,16 @@ public class TestConfiguration implements IConfiguration {
         podcasts.add(new ConfigurationNode("castcodersTest", "castcodersTest", "http://lescastcodeurs.libsyn.com/rss"));
     }
 
+    /* (non-Javadoc)
+     * @see net.holmes.core.configuration.IConfiguration#loadConfig()
+     */
     @Override
     public void loadConfig() {
     }
 
+    /* (non-Javadoc)
+     * @see net.holmes.core.configuration.IConfiguration#saveConfig()
+     */
     @Override
     public void saveConfig() {
     }
@@ -43,51 +70,81 @@ public class TestConfiguration implements IConfiguration {
         return contentFolder;
     }
 
+    /* (non-Javadoc)
+     * @see net.holmes.core.configuration.IConfiguration#getUpnpServerName()
+     */
     @Override
     public String getUpnpServerName() {
         return DEFAULT_UPNP_SERVER_NAME;
     }
 
+    /* (non-Javadoc)
+     * @see net.holmes.core.configuration.IConfiguration#setUpnpServerName(java.lang.String)
+     */
     @Override
     public void setUpnpServerName(String upnpServerName) {
 
     }
 
+    /* (non-Javadoc)
+     * @see net.holmes.core.configuration.IConfiguration#getHttpServerPort()
+     */
     @Override
     public Integer getHttpServerPort() {
         return DEFAULT_HTTP_SERVER_PORT;
     }
 
+    /* (non-Javadoc)
+     * @see net.holmes.core.configuration.IConfiguration#setHttpServerPort(java.lang.Integer)
+     */
     @Override
     public void setHttpServerPort(Integer httpServerPort) {
 
     }
 
+    /* (non-Javadoc)
+     * @see net.holmes.core.configuration.IConfiguration#getVideoFolders()
+     */
     @Override
     public List<ConfigurationNode> getVideoFolders() {
         return this.videoFolders;
     }
 
+    /* (non-Javadoc)
+     * @see net.holmes.core.configuration.IConfiguration#getPodcasts()
+     */
     @Override
     public List<ConfigurationNode> getPodcasts() {
         return this.podcasts;
     }
 
+    /* (non-Javadoc)
+     * @see net.holmes.core.configuration.IConfiguration#getAudioFolders()
+     */
     @Override
     public List<ConfigurationNode> getAudioFolders() {
         return this.audioFolders;
     }
 
+    /* (non-Javadoc)
+     * @see net.holmes.core.configuration.IConfiguration#getPictureFolders()
+     */
     @Override
     public List<ConfigurationNode> getPictureFolders() {
         return this.pictureFolders;
     }
 
+    /* (non-Javadoc)
+     * @see net.holmes.core.configuration.IConfiguration#getParameter(net.holmes.core.configuration.Parameter)
+     */
     @Override
     public Boolean getParameter(Parameter param) {
         return Boolean.FALSE;
     }
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
