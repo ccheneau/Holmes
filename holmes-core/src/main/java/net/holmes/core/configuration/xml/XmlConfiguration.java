775fbac4785fdebf7752c39ed3fe6e63263c19df
Cedric Cheneau <cedric.cheneau@free.fr>
2012-08-16 19:55:59 +0200

    Refactor XmlConfiguration

--- holmes-core/src/main/java/net/holmes/core/configuration/xml/XmlConfiguration.java
+++ holmes-core/src/main/java/net/holmes/core/configuration/xml/XmlConfiguration.java
@@ -23,7 +23,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.Serializable;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Properties;
 
 import net.holmes.core.configuration.ConfigurationNode;
 import net.holmes.core.configuration.IConfiguration;
@@ -227,5 +230,66 @@ public final class XmlConfiguration implements IConfiguration {
     @Override
     public String toString() {
         return this.rootNode.toString();
+    }
+
+    private final class XmlRootNode implements Serializable {
+        private static final long serialVersionUID = 1607439493422835211L;
+
+        private String upnpServerName;
+        private Integer httpServerPort;
+        private LinkedList<ConfigurationNode> videoFolders;
+        private LinkedList<ConfigurationNode> pictureFolders;
+        private LinkedList<ConfigurationNode> audioFolders;
+        private LinkedList<ConfigurationNode> podcasts;
+        private Properties parameters;
+
+        public void check() {
+            if (this.videoFolders == null) this.videoFolders = new LinkedList<ConfigurationNode>();
+            if (this.audioFolders == null) this.audioFolders = new LinkedList<ConfigurationNode>();
+            if (this.pictureFolders == null) this.pictureFolders = new LinkedList<ConfigurationNode>();
+            if (this.podcasts == null) this.podcasts = new LinkedList<ConfigurationNode>();
+            if (this.parameters == null) this.parameters = new Properties();
+            for (Parameter param : Parameter.values()) {
+                if (this.parameters.getProperty(param.getName()) == null) this.parameters.put(param.getName(), param.getDefaultValue());
+            }
+        }
+
+        public String getUpnpServerName() {
+            return this.upnpServerName;
+        }
+
+        public void setUpnpServerName(String upnpServerName) {
+            this.upnpServerName = upnpServerName;
+        }
+
+        public Integer getHttpServerPort() {
+            return this.httpServerPort;
+        }
+
+        public void setHttpServerPort(Integer httpServerPort) {
+            this.httpServerPort = httpServerPort;
+        }
+
+        public List<ConfigurationNode> getVideoFolders() {
+            return this.videoFolders;
+        }
+
+        public List<ConfigurationNode> getPodcasts() {
+            return this.podcasts;
+        }
+
+        public List<ConfigurationNode> getAudioFolders() {
+            return this.audioFolders;
+        }
+
+        public List<ConfigurationNode> getPictureFolders() {
+            return this.pictureFolders;
+        }
+
+        public Boolean getParameter(Parameter param) {
+            String value = (String) this.parameters.get(param.getName());
+            if (value == null) value = param.getDefaultValue();
+            return Boolean.parseBoolean(value);
+        }
     }
 }
