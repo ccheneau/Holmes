ad92a48fe9341639acd2588dc1c43ea50e3d27bb
Cedric Cheneau <cedric.cheneau@free.fr>
2013-02-17 19:49:39 +0100

    Refactoring

--- holmes-core/src/main/java/net/holmes/core/backend/_default/response/ConfigurationResponse.java
+++ holmes-core/src/main/java/net/holmes/core/backend/_default/response/ConfigurationResponse.java
@@ -14,7 +14,7 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-package net.holmes.core.backend.response;
+package net.holmes.core.backend._default.response;
 
 public class ConfigurationResponse {
     private final String serverName;
