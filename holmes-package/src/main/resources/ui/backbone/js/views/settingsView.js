777ed3998a33f39a56c22190298d15c84d219050
Cedric Cheneau <cedric.cheneau@free.fr>
2013-02-23 18:05:07 +0100

    Add backbone for settings management

--- holmes-package/src/main/resources/ui/backbone/js/views/settingsView.js
+++ holmes-package/src/main/resources/ui/backbone/js/views/settingsView.js
@@ -7,7 +7,7 @@ var Application = (function(application) {
 		},
 		render : function() {
 			var that = this;
-		    $.getJSON(this.url, function(response) {
+			$.getJSON(this.url, function(response) {
 				var renderedContent = Mustache.to_html(that.template, {
 					settings : response,
 					title : $.i18n.prop("msg.settings.title"),
@@ -19,21 +19,32 @@ var Application = (function(application) {
 					save : $.i18n.prop("msg.save")
 				});
 				that.$el.html(renderedContent);
-		    });
+			});
 		},
 		events : {
 			"click #btnSettingsSave" : "onSettingsSave",
 			"click #btnSettingsCancel" : "onSettingsCancel"
 		},
 		onSettingsSave : function() {
-	    	$.post(this.url,
-	        		{serverName : $("#settingsServerName").val().trim(),
-	        			httpServerPort : $("#settingsHttpServerPort").val().trim(),
-	        			prependPodcastItem : $("#chkPrependPodcastItem").is(':checked') ? "true":"false",
-	        			enableExternalSubtitles : $("#chkEnableExternalSubtitles").is(':checked') ? "true":"false"
-	        		})
-	        		.done(function() {$("#messagebox").message({text: $.i18n.prop("msg.settings.saved"), type: "success"});})
-	        		.fail(function(response) {$("#messagebox").message({text: response.responseText || response.statusText, type: "error"});});
+			new Application.Models.Settings().save({
+				"serverName" : $("#settingsServerName").val().trim(),
+				"httpServerPort" : $("#settingsHttpServerPort").val().trim(),
+				"prependPodcastItem" : $("#chkPrependPodcastItem").is(':checked') ? "true" : "false",
+				"enableExternalSubtitles" : $("#chkEnableExternalSubtitles").is(':checked') ? "true" : "false"
+			}, {
+				success : function() {
+					$("#messagebox").message({
+						text : $.i18n.prop("msg.settings.saved"),
+						type : "success"
+					});
+				},
+				error : function(model, response) {
+					$("#messagebox").message({
+						text : response.responseText || response.statusText,
+						type : "error"
+					});
+				}
+			});
 		},
 		onSettingsCancel : function() {
 			this.render();
