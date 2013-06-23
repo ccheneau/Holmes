a660636b1dd591c5e3e605b12304b40ece23b911
Cedric Cheneau <cedric.cheneau@free.fr>
2013-06-23 19:41:22 +0200

    Add junit tests

---
+++ holmes-core/src/test/java/net/holmes/core/backend/response/ConfigurationFolderTest.java
@@ -0,0 +1,95 @@
+package net.holmes.core.backend.response;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * ConfigurationFolder Tester.
+ */
+public class ConfigurationFolderTest {
+
+    /**
+     * Method: getId()
+     */
+    @Test
+    public void testGetId() throws Exception {
+        ConfigurationFolder folder = new ConfigurationFolder("id", "name", "path");
+        assertEquals(folder.getId(), "id");
+    }
+
+    /**
+     * Method: setId(final String id)
+     */
+    @Test
+    public void testSetId() throws Exception {
+        ConfigurationFolder folder = new ConfigurationFolder("id", "name", "path");
+        folder.setId("id2");
+        assertEquals(folder.getId(), "id2");
+    }
+
+    /**
+     * Method: getName()
+     */
+    @Test
+    public void testGetName() throws Exception {
+        ConfigurationFolder folder = new ConfigurationFolder("id", "name", "path");
+        assertEquals(folder.getName(), "name");
+    }
+
+    /**
+     * Method: setName(final String name)
+     */
+    @Test
+    public void testSetName() throws Exception {
+        ConfigurationFolder folder = new ConfigurationFolder("id", "name", "path");
+        folder.setName("name2");
+        assertEquals(folder.getName(), "name2");
+
+    }
+
+    /**
+     * Method: getPath()
+     */
+    @Test
+    public void testGetPath() throws Exception {
+        ConfigurationFolder folder = new ConfigurationFolder("id", "name", "path");
+        assertEquals(folder.getPath(), "path");
+    }
+
+    /**
+     * Method: setPath(final String path)
+     */
+    @Test
+    public void testSetPath() throws Exception {
+        ConfigurationFolder folder = new ConfigurationFolder("id", "name", "path");
+        folder.setPath("path2");
+        assertEquals(folder.getPath(), "path2");
+
+    }
+
+    /**
+     * Method: hashCode()
+     */
+    @Test
+    public void testHashCode() throws Exception {
+        ConfigurationFolder folder = new ConfigurationFolder("id", "name", "path");
+        ConfigurationFolder folder2 = new ConfigurationFolder("id", "name", "path");
+        ConfigurationFolder folder3 = new ConfigurationFolder("id3", "name", "path");
+        assertEquals(folder.hashCode(), folder2.hashCode());
+        assertNotEquals(folder.hashCode(), folder3.hashCode());
+    }
+
+    /**
+     * Method: equals(Object obj)
+     */
+    @Test
+    public void testEquals() throws Exception {
+        ConfigurationFolder folder = new ConfigurationFolder("id", "name", "path");
+        ConfigurationFolder folder2 = new ConfigurationFolder("id", "name", "path");
+        ConfigurationFolder folder3 = new ConfigurationFolder("id3", "name", "path");
+        assertEquals(folder, folder2);
+        assertNotEquals(folder, folder3);
+    }
+}
---
+++ holmes-core/src/test/java/net/holmes/core/backend/response/SettingsTest.java
@@ -0,0 +1,129 @@
+package net.holmes.core.backend.response;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Settings Tester.
+ */
+public class SettingsTest {
+    /**
+     * Method: getServerName()
+     */
+    @Test
+    public void testGetServerName() throws Exception {
+        Settings settings = new Settings("serverName", 999, true, true, true);
+        assertEquals(settings.getServerName(), "serverName");
+    }
+
+    /**
+     * Method: setServerName(final String serverName)
+     */
+    @Test
+    public void testSetServerName() throws Exception {
+        Settings settings = new Settings("serverName", 999, true, true, true);
+        settings.setServerName("newServerName");
+        assertEquals(settings.getServerName(), "newServerName");
+    }
+
+    /**
+     * Method: getHttpServerPort()
+     */
+    @Test
+    public void testGetHttpServerPort() throws Exception {
+        Settings settings = new Settings("serverName", 999, true, true, true);
+        assertTrue(settings.getHttpServerPort() == 999);
+    }
+
+    /**
+     * Method: setHttpServerPort(final Integer httpServerPort)
+     */
+    @Test
+    public void testSetHttpServerPort() throws Exception {
+        Settings settings = new Settings("serverName", 999, true, true, true);
+        settings.setHttpServerPort(1000);
+        assertTrue(settings.getHttpServerPort() == 1000);
+    }
+
+    /**
+     * Method: getPrependPodcastItem()
+     */
+    @Test
+    public void testGetPrependPodcastItem() throws Exception {
+        Settings settings = new Settings("serverName", 999, true, true, true);
+        assertTrue(settings.getPrependPodcastItem());
+    }
+
+    /**
+     * Method: setPrependPodcastItem(final Boolean prependPodcastItem)
+     */
+    @Test
+    public void testSetPrependPodcastItem() throws Exception {
+        Settings settings = new Settings("serverName", 999, true, true, true);
+        settings.setPrependPodcastItem(false);
+        assertFalse(settings.getPrependPodcastItem());
+    }
+
+    /**
+     * Method: getEnableExternalSubtitles()
+     */
+    @Test
+    public void testGetEnableExternalSubtitles() throws Exception {
+        Settings settings = new Settings("serverName", 999, true, true, true);
+        assertTrue(settings.getEnableExternalSubtitles());
+    }
+
+    /**
+     * Method: setEnableExternalSubtitles(final Boolean enableExternalSubtitles)
+     */
+    @Test
+    public void testSetEnableExternalSubtitles() throws Exception {
+        Settings settings = new Settings("serverName", 999, true, true, true);
+        settings.setEnableExternalSubtitles(false);
+        assertFalse(settings.getEnableExternalSubtitles());
+    }
+
+    /**
+     * Method: getHideEmptyRootNodes()
+     */
+    @Test
+    public void testGetHideEmptyRootNodes() throws Exception {
+        Settings settings = new Settings("serverName", 999, true, true, true);
+        assertTrue(settings.getHideEmptyRootNodes());
+    }
+
+    /**
+     * Method: setHideEmptyRootNodes(Boolean hideEmptyRootNodes)
+     */
+    @Test
+    public void testSetHideEmptyRootNodes() throws Exception {
+        Settings settings = new Settings("serverName", 999, true, true, true);
+        settings.setHideEmptyRootNodes(false);
+        assertFalse(settings.getHideEmptyRootNodes());
+    }
+
+    /**
+     * Method: hashCode()
+     */
+    @Test
+    public void testHashCode() throws Exception {
+        Settings settings = new Settings("serverName", 999, true, true, true);
+        Settings settings2 = new Settings("serverName", 999, true, true, true);
+        Settings settings3 = new Settings("serverName3", 999, true, true, true);
+        assertEquals(settings.hashCode(), settings2.hashCode());
+        assertNotEquals(settings.hashCode(), settings3.hashCode());
+    }
+
+    /**
+     * Method: equals(Object obj)
+     */
+    @Test
+    public void testEquals() throws Exception {
+        Settings settings = new Settings("serverName", 999, true, true, true);
+        Settings settings2 = new Settings("serverName", 999, true, true, true);
+        Settings settings3 = new Settings("serverName3", 999, true, true, true);
+        assertEquals(settings, settings2);
+        assertNotEquals(settings, settings3);
+    }
+}
