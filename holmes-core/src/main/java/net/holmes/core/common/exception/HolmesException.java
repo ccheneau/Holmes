2de59aaa2bd111669a77d941d966bbc6450bdd9b
Cedric Cheneau <cedric.cheneau@free.fr>
2014-05-21 19:11:06 +0200

    Remove unused constructors

--- holmes-core/src/main/java/net/holmes/core/common/exception/HolmesException.java
+++ holmes-core/src/main/java/net/holmes/core/common/exception/HolmesException.java
@@ -23,14 +23,6 @@ package net.holmes.core.common.exception;
 public class HolmesException extends Exception {
 
     /**
-     * Constructs a new exception with {@code null} as its detail message.
-     * The cause is not initialized, and may subsequently be initialized by a
-     * call to {@link #initCause}.
-     */
-    public HolmesException() {
-    }
-
-    /**
      * Constructs a new exception with the specified detail message.  The
      * cause is not initialized, and may subsequently be initialized by
      * a call to {@link #initCause}.
@@ -40,24 +32,6 @@ public class HolmesException extends Exception {
      */
     public HolmesException(String message) {
         super(message);
-    }
-
-    /**
-     * Constructs a new exception with the specified detail message and
-     * cause.  <p>Note that the detail message associated with
-     * {@code cause} is <i>not</i> automatically incorporated in
-     * this exception's detail message.
-     *
-     * @param message the detail message (which is saved for later retrieval
-     *                by the {@link #getMessage()} method).
-     * @param cause   the cause (which is saved for later retrieval by the
-     *                {@link #getCause()} method).  (A <tt>null</tt> value is
-     *                permitted, and indicates that the cause is nonexistent or
-     *                unknown.)
-     * @since 1.4
-     */
-    public HolmesException(String message, Throwable cause) {
-        super(message, cause);
     }
 
     /**
@@ -72,27 +46,8 @@ public class HolmesException extends Exception {
      *              {@link #getCause()} method).  (A <tt>null</tt> value is
      *              permitted, and indicates that the cause is nonexistent or
      *              unknown.)
-     * @since 1.4
      */
     public HolmesException(Throwable cause) {
         super(cause);
-    }
-
-    /**
-     * Constructs a new exception with the specified detail message,
-     * cause, suppression enabled or disabled, and writable stack
-     * trace enabled or disabled.
-     *
-     * @param message            the detail message.
-     * @param cause              the cause.  (A {@code null} value is permitted,
-     *                           and indicates that the cause is nonexistent or unknown.)
-     * @param enableSuppression  whether or not suppression is enabled
-     *                           or disabled
-     * @param writableStackTrace whether or not the stack trace should
-     *                           be writable
-     * @since 1.7
-     */
-    public HolmesException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
